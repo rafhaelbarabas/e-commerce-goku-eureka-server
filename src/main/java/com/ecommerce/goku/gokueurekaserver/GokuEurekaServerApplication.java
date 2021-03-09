a325a98b2a9dc31f29b6b8e3782899b1c55e7b94
rafhael.barabas <rafhael.barabas@betha.com.br>
2021-03-09 17:35:50 -0300

    initial

---
+++ src/main/java/com/ecommerce/goku/gokueurekaserver/GokuEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.ecommerce.goku.gokueurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GokuEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GokuEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommerce/goku/gokueurekaserver/GokuEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.goku.gokueurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GokuEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
